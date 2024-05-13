c06b9f90da0527232bbee2c2feb913890942be83
Max <156679584+TakaTikiTonga@users.noreply.github.com>
2024-05-13 12:27:52 +0200

    Add files via upload

---
+++ SUD/LF5/DB/DBOtto/LS7/Transaktionen.sql
@@ -0,0 +1,32 @@
+START TRANSACTION;
+-- GANZ VIEL TOLLER CODE
+COMMIT; -- Erst jetzt ändert sich der Datenstand
+
+START TRANSACTION;
+
+INSERT INTO kdauftragsposition (FKAuftrag, FKArtikel, Anzahl)
+VALUES (11085, 1, 1);
+
+UPDATE artikel
+SET Lagerbestand = Lagerbestand -1
+    WHERE ArtikelNr = 1;
+
+COMMIT;
+
+
+-- A7
+-- Erstellen Sie eine Transaktion, welche ein Insert auf eine Lieferauftragsposition
+-- zum Lieferauftrag 1083 zusammen mit dem passenden Update auf die Tabelle Artikel ausführt.
+-- Nutzen Sie dafür den Artikel Nummer 14.
+
+
+START TRANSACTION;
+
+INSERT INTO kdauftragsposition (FKAuftrag, FKArtikel, Anzahl)
+VALUES (1083, 14, 5);
+
+UPDATE artikel
+SET Lagerbestand = Lagerbestand + 5
+    WHERE ArtikelNr = 14;
+
+COMMIT;
