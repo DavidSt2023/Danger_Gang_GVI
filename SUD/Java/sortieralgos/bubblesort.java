a833dbc3c590c4e43905116060fd1f416fcee9fc
Lukas <118361150+lks1802@users.noreply.github.com>
2024-10-10 12:01:22 +0200

    feat(JAVA): update sortieralgos

--- SUD/Java/sortieralgos/bubblesort.java
+++ SUD/Java/sortieralgos/bubblesort.java
@@ -8,13 +8,18 @@ import java.util.Random;
 public class bubblesort {
 
   public static void bubbleSort(int[] arr) {
-    for (int i = 1; i < arr.length; i++) {
-      for (int j = 0; j < arr.length - 1; j++) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      boolean swapped = false;
+      for (int j = 0; j < arr.length - 1 - i; j++) {
         if (arr[j] > arr[j + 1]) {
           int temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
+          swapped = true;
         }
+      }
+      if (!swapped) {
+        break;
       }
     }
   }
